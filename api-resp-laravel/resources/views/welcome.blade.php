699164e6b34bd32aad05fb78de930b0891ac8f6d
Alejo <rafael.valencia.r4@gmail.com>
2021-07-09 08:40:34 -0500

    test

--- api-resp-laravel/resources/views/welcome.blade.php
+++ api-resp-laravel/resources/views/welcome.blade.php
@@ -84,7 +84,7 @@
                     Laravel
                 </div>
 
-                <p>Hola mundo </p>
+                <p>Hola mundo 3 </p>
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
                     <a href="https://laracasts.com">Laracasts</a>
